ab802962c7397dd63dda212d8f8bc26a15b2020a
Inuka Shanesh de Costha <inuka@iShanesh-Mac.local>
2019-09-26 11:18:23 +0200

    add wordcout feature

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='word_counter',
+i    name='word_counter',
     version='0.1',
-    author_email='radzhome@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/radlws/python-word-counter',
     license='',
     description='A simple word counter.',
     long_description=open('README.md').read(),
